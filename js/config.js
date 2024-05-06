85975e6fa454434e83ab445bdb5ec8e07e19b5f6
Thunderkegl33t <169084683+Thunderkegl33t@users.noreply.github.com>
2024-05-06 22:34:38 +0500

    |
    |

---
+++ js/config.js
@@ -0,0 +1,53 @@
+/* 
+  Конифг для твоего сайта
+  Можешь менять данные
+*/
+
+let config = {
+
+  // Задержка загрузки (1 секунда)
+  preloaderDelay: 2,
+
+  // Задержка появления видео (4.2 секунды)
+  showDelay: 20.5, 
+
+  // Заголовок
+  title: 'Nedokoder Azizbek',
+
+  // Социальные сети
+  social: {
+    vk: 'lo1se228',
+    steam: 'thunderkegl33t',
+    telegram: '@TPaBa_He_HaPKoTuk26',
+  },
+
+
+
+
+
+  // Цвет иконки загрузки
+  preloaderColor: '#FF0000',
+
+
+  // Цвет кнопки play
+  playButtonColor: '#FF0000',
+  // Цвет тени кнопки play
+  playButtonShadowColor: '#313036',
+
+
+  // Цвет ee00ff
+  titleColor: '#FF0000',
+  // Цвет тени загаловка
+  titleShadowColor: '#313036',
+
+
+  // Текст копирайта
+  copyrightText: 'Сделано в пещере, Азизбеком.',
+  // Цвет копирайта
+  copyrightTextColor: '#FF0000',
+
+  // Цвет иконок социальных сетей
+  socialColor: '#FF0000',
+  // Цвет тени иконок социальных сетей
+  socialShadowColor: '#313036',
+}
